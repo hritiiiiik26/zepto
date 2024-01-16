987f6ae2a230f92c3c90505722021c5f7916cc10
hrixtik <Hritik.Kumar@cogoport.com>
2024-01-16 15:35:18 +0530

    styling fixes

--- src/components/AutocompleteChips.js
+++ src/components/AutocompleteChips.js
@@ -4,11 +4,13 @@ import { names } from "../utils/constants";
 import img from "../no-result-found.jpg";
 
 const AutocompleteChips = () => {
+  const [show, setShow] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [selectedItems, setSelectedItems] = useState([]);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
+    setShow(true);
   };
 
   const handleItemClick = (item) => {
@@ -28,7 +30,11 @@ const AutocompleteChips = () => {
   );
 
   return (
-    <div className="autocomplete-container">
+    <div
+      className="autocomplete-container"
+      aria-hidden
+      onClick={() => setShow(true)}
+    >
       <div className="chips-input-container">
         <div className="chips-container">
           {selectedItems.map((item) => (
@@ -46,17 +52,21 @@ const AutocompleteChips = () => {
         />
       </div>
 
-      {filteredItems.length > 0 ? (
-        <ul className="suggestions-list">
-          {filteredItems.map((item) => (
-            <li key={item} onClick={() => handleItemClick(item)}>
-              {item}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <div className="img">
-          <img src={img} alt="/" width="100%" />
+      {show && (
+        <div className="suggestions-container">
+          {filteredItems.length > 0 ? (
+            <ul className="suggestions-list">
+              {filteredItems.map((item) => (
+                <li key={item} onClick={() => handleItemClick(item)}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="img">
+              <img src={img} alt="No Result Found" width="100%" />
+            </div>
+          )}
         </div>
       )}
     </div>
